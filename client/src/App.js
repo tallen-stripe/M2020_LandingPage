7e1dc52ee9b29c50dbe555f47863d047182458d1
tallen-stripe <tallen@stripe.com>
2022-05-10 18:22:48 +0100

    Pre Run Command

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <ul className="divide-y divide-gray-200">
-        <li className="py-4 flex">
-          <img className="h-10 w-10 rounded-full" alt="" />
-          <div className="ml-3">
-            <p className="text-sm font-medium text-gray-900">Tyler</p>
-            <p className="text-sm text-gray-500">tallen@stripe.com</p>
-          </div>
-        </li>
-      </ul>
+    <div>
+        <Footer></Footer>
     </div>
   );
 }
